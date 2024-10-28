80c642e058022ba1788bcc4f4ace2082727d6f8a
lipenathan <felipenatthan@gmail.com>
2024-10-27 23:03:49 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# filmes
+
+Aplicação de desafio iniciado na aula do dia 24/10/2024 na aula de Desenvolvimento Web - Utilizando Frameworks
+
+Desafio:
+
+Escolha um tema da sua preferência, como, Futebol, Basquete, Filmes, Séries, etc.
+Crie um componente personalizado.
+Crie um objeto para o tema contendo pelo menos 3 atributos.
+Receba esses atributos do usuário, cadastre o item em uma lista.
+A tela/componente deve conter, pelo menos:
+Os inputs.
+- Botão para cadastro
+- Botões para deleção individual do item.
+- Caso a lista esteja vazia, tratar e mostrar uma mensagem ao usuário.
+- Fazer tratamentos dos campos e limpá-los após completo.
+
+
+Executar o projeto de forma local
+
+npm install
+npm run dev
---
+++ src/components/Filmes.vue
@@ -0,0 +1,152 @@
+<template>
+    <div class="titulo">
+        <h2>Bons Filmes</h2>
+    </div>
+    <div class="body">
+        <label>
+            Título
+            <input v-model="titulo" type="text" placeholder="Título" required>
+        </label>
+
+        <label>
+            Ano
+            <input v-model="ano" type="number" placeholder="Ano" required>
+        </label>
+
+        <label>
+            Diretor
+            <input @keyup.enter="submeter" v-model="diretor" type="text" placeholder="Diretor" required>
+            <!-- Modificador de evento para escutar o evento keyup da tecla enter -->
+        </label>
+
+        <button @click="submeter"> Cadastrar Filme </button>
+
+        <div class="filmes">
+            <template v-if="filmes.length == 0">
+                <p> Lista vazia </p>
+            </template>
+            <template v-else>
+                <div>
+                    <div class="item-filme" v-for="filme in filmes" :key="filme.id"> <!-- passando id para o :key, assim o vue consegue indexar e identificar melhor os itens da lista -->
+                        <p>Título: {{ filme.titulo }}|| Ano: {{ filme.ano }} || Diretor: {{ filme.diretor }}</p><button
+                            @click="deletar(filme.id)" class="excluir">Excluir</button> <!-- Chamando o método de exclusão passando o id do item -->
+                    </div>
+                </div>
+            </template>
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref, reactive } from 'vue'
+//ref para atributos reativos simples
+//reactive para atributos reativos complexos
+
+const titulo = ref("")
+const ano = ref("")
+const diretor = ref("")
+var filmes = reactive([])
+var id = 0 //não reativa. variável que será utilizada somente para lógica de atribuir ids aos componentes
+
+function submeter() { //método que realiza cadastro de filme, utilizando valores reativos que usuário digitou
+    if (titulo.value.length == 0) {
+        alert('Preencha o nome do filme')
+        return
+    }
+
+    if (ano.value.toString().length != 4) {
+        alert('Ano precisa conter 4 dígitos')
+        return
+    }
+
+    if (diretor.value.length == 0) {
+        alert('Preencha o diretor do filme')
+        return
+    }
+
+    const filme = {
+        'id': ++id, //id incremental(incrementa e então atribui)
+        'titulo': titulo.value,
+        'ano': ano.value,
+        'diretor': diretor.value
+    }
+
+    titulo.value = ""
+    ano.value = ""
+    diretor.value = ""
+
+    filmes.push(filme)
+}
+
+function deletar(id) {//método para deletar item de acordo com o id
+    var index = filmes.findIndex(filme => filme.id === id )
+    filmes.splice(index, 1)
+}
+
+</script>
+<style>
+* {
+    margin: 0;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+.titulo {
+    background-color: blue;
+    color: black;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 10vh;
+}
+
+.titulo>h2 {
+    display: inline;
+    text-align: start;
+    margin-left: 4vh;
+}
+
+.body {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10vh;
+    height: 100vh;
+    text-align: center;
+}
+
+label {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    text-align: left;
+    width: 100%;
+    max-width: 300px;
+}
+
+input {
+    width: 100%;
+    padding: 8px;
+    margin-top: 5px;
+    box-sizing: border-box;
+}
+
+button {
+    margin-top: 20px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+.filmes {
+    margin-top: 5vh;
+    text-align: center;
+}
+
+.item-filme p {
+    display: inline;
+}
+
+.excluir {
+    padding: 0;
+    margin-left: 6vh;
+}
+</style>
